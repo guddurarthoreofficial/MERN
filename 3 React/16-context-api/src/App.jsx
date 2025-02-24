78a7ebefee538e541b08ea8332484604f6bd84e1
Guddu Kumar <guddu748081@gmail.com>
2025-02-24 11:16:57 +0530

    theme function is added

--- 3 React/16-context-api/src/App.jsx
+++ 3 React/16-context-api/src/App.jsx
@@ -1,8 +1,22 @@
+import { createContext, useContext, useState } from "react";
+import { Welcome } from "./components/Welcome";
+import { ThemeToggle } from "./components/ThemeToggle";
+import ThemeContext from "./components/store/ThemeContext";
+
 function App() {
+
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () =>{
+    setTheme(currentTheme => currentTheme === 'light' ? 'dark' : 'light');
+  }
 
   return (
     <>
-    <div className="text-red-600">Learnig context Api</div>
+      <ThemeContext.Provider value={{theme:theme, toggleTheme:toggleTheme }}>
+        <Welcome/>
+        <ThemeToggle/>
+      </ThemeContext.Provider>
     </>
   )
 }
---
+++ 3 React/16-context-api/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react'
+import ThemeContext from './store/ThemeContext'
+
+export const Welcome = () => {
+    const { theme } = useContext(ThemeContext)
+    console.log(theme);
+    return (
+        <div className={`text-3xl m-3 p-2 ${theme === 'light' ? "bg-slate-200 text-gray-800" : "text-slate-200 bg-gray-800" }`}>Welcome to Theme Changing App.</div>
+    )
+}
